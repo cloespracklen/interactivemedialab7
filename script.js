86357b4ecc672e04d3ce2abcffebba3007dedba8
cloespracklen <cloe.spracklen@centre.edu>
2020-09-11 11:35:47 -0400

    interactivemediaupdate

---
+++ script.js
@@ -0,0 +1,19 @@
+d3.select("#officeholiday")
+.on("click", function()
+    {
+    d3.select("#officecastpic")
+.classed("hidden",false)
+})
+
+d3.select("#dwight")
+.on("click", function()
+   {
+    d3.select("#beets")
+   .classed("hidden", false)
+   })
+
+d3.select("#andy")
+.on("click", function()
+   {d3.select("#radish")
+   .classed("hidden", false)
+   })
